843ad648bf5551cb3fdda15befa83083ce05f329
qsrdev <matteo.albertini747@gmail.com>
2025-04-17 23:02:37 +0200

    first commit

---
+++ esercizio mail/js/script.js
@@ -0,0 +1,17 @@
+const mailUser = prompt("Inserisci la mail per l'accesso");
+console.log(mailUser);
+
+const mailList = ["utente@gmail.com", "utente1@gmail.com", "utente2@gmail.com"];
+
+for (let i = 0; i < mailList.length; i++) {
+  const mail = mailList[i];
+  console.log(mail);
+
+  if (mail === mailUser) {
+    console.log("Mail riconosciuta accesso in corso");
+    break;
+  } else {
+    console.log("Mail non riconosciuta accesso negato");
+    break;
+  }
+}
